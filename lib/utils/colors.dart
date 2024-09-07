7725e7c7abdbba3c074176f541232704617c2b69
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-07 13:04:46 +0500

    update

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppColorsLight {
-  static Color primary = const Color(0xFFFFFFFF);
-  static Color secondary = const Color(0xFF704F38);
-  static Color tertiary = const Color(0xFF282932);
+  static String? _theme;
+  static void _saveThemeMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('Theme', 'light');
+    _theme = prefs.getString('Theme');
+  }
+
+  static Color get primary => const Color(0xFFFFFFFF);
+  static Color get secondary => const Color(0xFF704F38);
+  static Color get tertiary => const Color(0xFF282932);
 }
