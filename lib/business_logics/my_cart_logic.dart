f09aa35d9808195fc213e6111c16dc0a5f3ddf15
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-13 20:59:36 +0500

    update

--- lib/business_logics/my_cart_logic.dart
+++ lib/business_logics/my_cart_logic.dart
@@ -1,5 +1,56 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyCart {
-    final SharedPreferences
+  Future<void> createMyCart() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setStringList('myCart', []);
+  }
+
+  Future<List<String>> getCartData() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final List<String>? myCartData = sp.getStringList('myCart');
+    if (myCartData == null) {
+      await createMyCart();
+      final List<String> temp = sp.getStringList('myCart')!;
+      return temp;
+    } else {
+      return myCartData;
+    }
+  }
+
+  Future<bool> addToMyCart({required int productId}) async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final List<String>? myCartData = sp.getStringList('myCart');
+    try {
+      if (myCartData == null) {
+        await createMyCart();
+        final List<String> temp = sp.getStringList('myCart')!;
+        temp.add(productId.toString());
+        sp.setStringList('myCart', temp);
+        return true;
+      } else {
+        myCartData.add(productId.toString());
+        sp.setStringList('myCart', myCartData);
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> deleteMyCart({required int productId}) async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final List<String>? myCartData = sp.getStringList('myCart');
+    try {
+      if (myCartData == null) {
+        return false;
+      } else {
+        myCartData.remove(productId.toString());
+        sp.setStringList('myCart', myCartData);
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
 }
