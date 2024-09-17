f3f6ea4e32fae4ba602630b2b5bf7c579d56c479
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-17 18:18:58 +0500

    update

--- lib/screens/signin_login_screen/verify_code_screen.dart
+++ lib/screens/signin_login_screen/verify_code_screen.dart
@@ -17,6 +17,12 @@ class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
   late String _email;
   bool _isLoading = false;
 
+  // Declare FocusNodes for each OTP field
+  final FocusNode _otp1FocusNode = FocusNode();
+  final FocusNode _otp2FocusNode = FocusNode();
+  final FocusNode _otp3FocusNode = FocusNode();
+  final FocusNode _otp4FocusNode = FocusNode();
+
   Future<void> _getEmail() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     _email = sp.getString('userEmail')!;
@@ -27,8 +33,15 @@ class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
 
   @override
   void initState() {
-    super.initState;
+    super.initState();
     _getEmail();
+  }
+
+  // Function to move focus to the next field
+  void _moveToNextField(
+      {required FocusNode currentFocus, required FocusNode nextFocus}) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
   }
 
   @override
@@ -37,65 +50,193 @@ class _VerifyCodeScreenState extends State<VerifyCodeScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
-        child: Column(children: [
-          Text(
-            CreateAccountScreenText.verifyCodeTitle,
-            style: TextStyle(
-              color: AppColors.tertiary,
-              fontSize: 40,
-              fontWeight: FontWeight.w500,
+        child: Column(
+          children: [
+            Text(
+              CreateAccountScreenText.verifyCodeTitle,
+              style: TextStyle(
+                color: AppColors.tertiary,
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          _isLoading
-              ? TextHighted(
-                  text:
-                      '${CreateAccountScreenText.verifyCodeWelcomeText} \n $_email',
-                  fontSize: 17,
-                  highlightColor: AppColors.secondary,
-                  normalTextColor: AppColors.textColorSubtitles,
-                  textToHighlight: [_email],
-                  textAlign: TextAlign.center,
-                )
-              : const Text(''),
-          const SizedBox(height: 80),
-          Form(
-            key: _formKey,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                  vertical: 0, horizontal: screenWidth * .1),
-              child: Row(
-                children: List<Widget>.generate(
-                    4,
-                    (index) => Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: TextFormField(
-                              textAlign: TextAlign.center,
-                              maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                              decoration: InputDecoration(
-                                hintText: '-',
-                                hintStyle: TextStyle(
-                                  color: AppColors.tertiary,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(50),
-                                  borderSide: BorderSide(
-                                    color: AppColors.textColorSubtitles
-                                        .withOpacity(.20),
-                                    width: 4,
-                                  ),
-                                ),
+            const SizedBox(height: 20),
+            _isLoading
+                ? TextHighted(
+                    text:
+                        '${CreateAccountScreenText.verifyCodeWelcomeText} \n $_email',
+                    fontSize: 17,
+                    highlightColor: AppColors.secondary,
+                    normalTextColor: AppColors.textColorSubtitles,
+                    textToHighlight: [_email],
+                    textAlign: TextAlign.center,
+                  )
+                : const Text(''),
+            const SizedBox(height: 80),
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: 0, horizontal: screenWidth * .1),
+                child: Row(
+                  children: [
+                    // OTP Field 1
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: TextFormField(
+                          focusNode: _otp1FocusNode,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          onChanged: (value) {
+                            if (value.length == 1) {
+                              _moveToNextField(
+                                  currentFocus: _otp1FocusNode,
+                                  nextFocus: _otp2FocusNode);
+                            }
+                          },
+                          decoration: InputDecoration(
+                            hintText: '-',
+                            hintStyle: TextStyle(
+                              color: AppColors.tertiary,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColors.textColorSubtitles
+                                    .withOpacity(.20),
+                                width: 4,
                               ),
                             ),
                           ),
-                        )),
+                        ),
+                      ),
+                    ),
+                    // OTP Field 2
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: TextFormField(
+                          focusNode: _otp2FocusNode,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          onChanged: (value) {
+                            if (value.length == 1) {
+                              _moveToNextField(
+                                  currentFocus: _otp2FocusNode,
+                                  nextFocus: _otp3FocusNode);
+                            }
+                          },
+                          decoration: InputDecoration(
+                            hintText: '-',
+                            hintStyle: TextStyle(
+                              color: AppColors.tertiary,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColors.textColorSubtitles
+                                    .withOpacity(.20),
+                                width: 4,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // OTP Field 3
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: TextFormField(
+                          focusNode: _otp3FocusNode,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          onChanged: (value) {
+                            if (value.length == 1) {
+                              _moveToNextField(
+                                  currentFocus: _otp3FocusNode,
+                                  nextFocus: _otp4FocusNode);
+                            }
+                          },
+                          decoration: InputDecoration(
+                            hintText: '-',
+                            hintStyle: TextStyle(
+                              color: AppColors.tertiary,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColors.textColorSubtitles
+                                    .withOpacity(.20),
+                                width: 4,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // OTP Field 4
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: TextFormField(
+                          focusNode: _otp4FocusNode,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          onChanged: (value) {
+                            if (value.length == 1) {
+                              _otp4FocusNode
+                                  .unfocus(); // Unfocus after last input
+                            }
+                          },
+                          decoration: InputDecoration(
+                            hintText: '-',
+                            hintStyle: TextStyle(
+                              color: AppColors.tertiary,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                              borderSide: BorderSide(
+                                color: AppColors.textColorSubtitles
+                                    .withOpacity(.20),
+                                width: 4,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
