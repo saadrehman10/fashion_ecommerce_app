78647dece0e31a4a82e8574fc4cce5a2bb1961e7
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-21 03:57:42 +0500

    update

--- lib/screens/welcome_screens/splash_screen.dart
+++ lib/screens/welcome_screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
-//
 import 'package:fashion_ecommerce_app/screens/welcome_screens/welcome_screen.dart';
 import 'package:fashion_ecommerce_app/utils/colors.dart';
 import 'package:fashion_ecommerce_app/utils/images.dart';
@@ -11,11 +10,12 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterSplashScreen.fadeIn(
-        duration: const Duration(seconds: 2),
-        backgroundColor: AppColors.primary,
-        nextScreen: const WelcomeScreen(),
-        childWidget: SizedBox(
-          child: Image.asset(AppImages.appIcon),
-        ));
+      duration: const Duration(seconds: 2),
+      backgroundColor: AppColors.primary,
+      nextScreen: const WelcomeScreen(),
+      childWidget: SizedBox(
+        child: Image.asset(AppImages.appIcon),
+      ),
+    );
   }
 }
