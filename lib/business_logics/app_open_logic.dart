055a2ad4bf5309b7b1c738ebc798ec9af7b43dc9
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-22 00:13:41 +0500

    update

--- lib/business_logics/app_open_logic.dart
+++ lib/business_logics/app_open_logic.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class AppOpenLogic {
+  static Future<void> setAppStatus(bool value) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setBool('firstOpen', value);
+  }
+
+  static Future<bool> checkAppStatus() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    bool? appStatue = sp.getBool('firstOpen');
+    if (appStatue == null) {
+      await setAppStatus(true);
+      return true;
+    } else {
+      debugPrint(appStatue.toString());
+      return appStatue;
+    }
+  }
+}
--- lib/screens/welcome_screens/splash_screen.dart
+++ lib/screens/welcome_screens/splash_screen.dart
@@ -1,21 +1,48 @@
+import 'package:fashion_ecommerce_app/business_logics/app_open_logic.dart';
+import 'package:fashion_ecommerce_app/screens/layout_page/layout_page.dart';
 import 'package:flutter/material.dart';
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:fashion_ecommerce_app/screens/welcome_screens/welcome_screen.dart';
 import 'package:fashion_ecommerce_app/utils/colors.dart';
 import 'package:fashion_ecommerce_app/utils/images.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late bool _nextScreenStatus;
+  bool _isLoading = false;
+
+  Future<void> _checkOpen() async {
+    _nextScreenStatus = await AppOpenLogic.checkAppStatus();
+    await AppOpenLogic.setAppStatus(false);
+    setState(() {
+      _isLoading = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkOpen();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FlutterSplashScreen.fadeIn(
-      duration: const Duration(seconds: 2),
-      backgroundColor: AppColors.primary,
-      nextScreen: const WelcomeScreen(),
-      childWidget: SizedBox(
-        child: Image.asset(AppImages.appIcon),
-      ),
-    );
+    return _isLoading
+        ? FlutterSplashScreen.fadeIn(
+            duration: const Duration(seconds: 2),
+            backgroundColor: AppColors.primary,
+            nextScreen:
+                _nextScreenStatus ? const WelcomeScreen() : const LayoutPage(),
+            childWidget: SizedBox(
+              child: Image.asset(AppImages.appIcon),
+            ),
+          )
+        : const Text('');
   }
 }
